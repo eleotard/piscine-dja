9ccd2e57fef9d646cf2add2774b65f7ba4c0d3fe
Elsie LEOTARD <lotardelsie@gmail.com>
2025-07-25 10:22:28 +0000

    ex07

--- d01/ex07/periodic_table.py
+++ d01/ex07/periodic_table.py
@@ -2,7 +2,11 @@ import sys
 
 # def read_file(file):
 #     with open(file, r) as periodic_file
+SOURCE_FILE = "periodic_table.txt"
+CSS_FILE = "style.css"
+HTML_FILE = "periodic_table.html"
 
+            # <div style="overflow-x:auto">
 def create_html_file():
     html_document = f"""
     <!DOCTYPE html>
@@ -10,34 +14,60 @@ def create_html_file():
         <head>
             <meta charset="UTF-8">
             <title>Periodic Table</title>
-            <style>
-            table {
-                border-collapse: collapse;
-            }
-            th, td {
-                border: 1px solid #000000;
-                text-align: middle;
-            }
-        </style>
+            <link rel="stylesheet" href={CSS_FILE}>
         </head>
         <body>
-            <h1>Periodic Table of Elements</h1>
+            <h1>PERIODIC TABLE OF ELEMENTS</h1>
             <table>
-
-            </table>
-
-        </body>
-        
-
-    </html>
     """
-    with open("periodic_table.html", "w") as file:
+    col = 0
+    for col in range(0, 8):
+        row = 0
+        html_document += "<tr>\n"
+        for r in range(0, 18):
+            if (col == 0):
+                if ((r == 0) or (r == 17)):
+                    html_document += "<td></td>\n"
+                else:
+                    html_document += "<td style=\"border: 0px solid #000000; padding: 50px;\"></td>\n"
+            elif ((col == 1) or (col == 2)):
+                if ((0 <= r <= 1) or (11 <= r <= 17)):
+                    html_document += "<td></td>\n"
+                else:
+                    html_document += "<td style=\"border: 0px solid #000000; padding: 50px;\"></td>\n"
+            else:
+                    html_document += "<td></td>\n"
+    html_document += "</tr>\n</table>\n</body>\n<html>\n"
+
+     
+    with open(HTML_FILE, "w") as file:
         file.write(html_document)
 
-
+        # width: 100%;
+        # table-layout: auto;
+def generate_css():
+    css_document = f"""
+    table {{
+        margin: auto;
+        border-collapse: collapse;
+    }}
+    th, td {{
+                border: 1px solid #000000;
+                padding: 50px;
+                text-align: middle;
+            }}
+    h1 {{
+        text-align: center;
+        font-family: 'Arial', sans-sherif;
+    }}
+    """
+    with open(CSS_FILE, "w") as file:
+        file.write(css_document)
 
 if __name__ == "__main__":
     #periodic_file = read_file("periodic_table.txt")
-    with open("periodic_table.txt", "r") as periodic_file:
+    generate_css()
+    with open(SOURCE_FILE, "r") as periodic_file:
         print(periodic_file.read())
+    create_html_file()
     # ft()
